197a8acb22a52d83d56254fe0330361215faaa81
Bill Kable <bkable@gmail.com>
2020-02-05 00:41:11 -0600

    Kyle - Configuring an App Task

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,4 +1,24 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.annotation.Retention;
+
+@RestController
 public class WelcomeController {
+
+    private final String welcomeMessage;
+
+    public WelcomeController(@Value("${welcome.message:NOT SET}") String message) {
+        this.welcomeMessage = message;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return welcomeMessage;
+    }
 }
