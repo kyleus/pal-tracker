83b186789f5d261ee1900398dfe56c7376be62f5
Kyle Ryan <kyle.ryan@cognizant.com>
2020-03-17 15:12:42 -0500

    kyle - added java.sql.Statement.RETURN_GENERATED_KEYS

--- src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
@@ -12,6 +12,8 @@ import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.List;
 
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+
 public class JdbcTimeEntryRepository implements TimeEntryRepository {
 
     private final JdbcTemplate jdbcTemplate;
